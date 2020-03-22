c6378617b4b850847df476866b6936776f41a186
Kowsur Ahmed <kowsurahmed@gmail.com>
2020-03-22 16:14:26 +0000

    hide other pages

--- kwauk/views.py
+++ kwauk/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import HttpResponse, render
 from django.contrib.auth.models import User
 from member.models import *
 def index(request):
+    return redirect("/coronahelp/")
     try:
         users = Member.objects.filter(user__is_active=True)
     except:
