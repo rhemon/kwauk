0f80a78130098435a180ce2446ab13795126eb19
Ridhwanul Haque <rhemon19@gmail.com>
2020-04-13 13:41:26 +0100

    edit fname, lname, phone

--- admin/views.py
+++ admin/views.py
@@ -173,10 +173,10 @@ def project_donation_commits(request, pid):
     if request.method == "POST":
         donation = ProjectDonations.objects.get(id=request.POST.get("donation_id"))
         u = donation.user
-        user.first_name = request.POST.get("first_name")
-        user.last_name = request.POST.get("last_name")
-        user.username = request.POST.get("phone")
-        user.save()
+        u.first_name = request.POST.get("first_name")
+        u.last_name = request.POST.get("last_name")
+        u.username = request.POST.get("phone")
+        u.save()
         donation.amount = request.POST.get("amount")
         donation.paid = request.POST.get("paid") != None
         donation.remarks = request.POST.get("remarks")
